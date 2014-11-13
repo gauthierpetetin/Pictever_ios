24e8eefca1a022899483216666045841c4b55c07
gauthierpetetin <gauthierpetetin@hotmail.com>
2014-11-13 14:12:26 +0100

    modifs iphone6 + reset password

---
+++ Pictever/Pictever/Inscription/PasswordRecovery.h
@@ -0,0 +1,13 @@
+//
+//  PasswordRecovery.h
+//  Pictever
+//
+//  Created by Gauthier Petetin on 12/11/2014.
+//  Copyright (c) 2014 Pictever. All rights reserved.
+//
+
+#ifndef Pictever_PasswordRecovery_h
+#define Pictever_PasswordRecovery_h
+
+
+#endif
---
+++ Pictever/Pictever/Inscription/PasswordRecovery.m
@@ -0,0 +1,9 @@
+//
+//  PasswordRecovery.m
+//  Pictever
+//
+//  Created by Gauthier Petetin on 12/11/2014.
+//  Copyright (c) 2014 Pictever. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
